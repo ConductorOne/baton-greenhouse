d6e269eca6a8f80051bba5228e3e36b770117bfc
Javier David Carnelli <javier.carnelli@conductorone.com>
2025-03-07 16:59:55 -0300

    feat: roles changed to create only the site admin role. Role grants listed according to that

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -4,13 +4,12 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
-	"net/http"
-	"net/url"
-
 	"github.com/conductorone/baton-greenhouse/pkg/models"
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 	"github.com/conductorone/baton-sdk/pkg/uhttp"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
+	"net/http"
+	"net/url"
 )
 
 var DefaultHost = "https://harvest.greenhouse.io"
@@ -57,61 +56,6 @@ func (c *Client) ListUsers(ctx context.Context, next string) ([]models.User, *v2
 	}
 
 	var target []models.User
-	if next != "" {
-		qurl = next
-	}
-	parsedURL, err := url.Parse(qurl)
-	if err != nil {
-		return nil, nil, "", fmt.Errorf("cannot parse URL, error: %w", err)
-	}
-
-	request, err := c.httpClient.NewRequest(ctx, http.MethodGet, parsedURL, uhttp.WithAcceptJSONHeader(), withBasicAuth(makeAuthorization(c.user)))
-	if err != nil {
-		return nil, nil, "", err
-	}
-
-	rl := v2.RateLimitDescription{}
-	doOptions := []uhttp.DoOption{
-		uhttp.WithRatelimitData(&rl),
-		uhttp.WithJSONResponse(&target),
-	}
-
-	resp, err := c.httpClient.Do(request, doOptions...)
-	if err != nil {
-		return nil, nil, "", err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, nil, "", fmt.Errorf("API return with unexpected status code %d %s", resp.StatusCode, resp.Status)
-	}
-	// see https://developers.greenhouse.io/harvest.html#pagination
-	link := &models.Link{}
-	err = link.UnmarshalText([]byte(resp.Header.Get("Link")))
-	if err != nil {
-		return nil, nil, "", fmt.Errorf("cannot unmarshal value of header Link, error: %w", err)
-	}
-
-	return target, &rl, link.Next, nil
-}
-
-func (c *Client) ListRoles(ctx context.Context, next string) ([]models.Role, *v2.RateLimitDescription, string, error) {
-	joinedURL, err := url.JoinPath(c.baseURL, "v1/user_roles")
-	if err != nil {
-		return nil, nil, "", err
-	}
-
-	params := map[string]interface{}{
-		"per_page":        500,
-		"page":            1,
-		"user_attributes": true,
-	}
-	qurl, err := urlAddQuery(joinedURL, params)
-	if err != nil {
-		return nil, nil, "", err
-	}
-
-	var target []models.Role
 	if next != "" {
 		qurl = next
 	}
--- pkg/connector/roles.go
+++ pkg/connector/roles.go
@@ -3,12 +3,13 @@ package connector
 import (
 	"context"
 	"fmt"
-	"github.com/conductorone/baton-greenhouse/pkg/models"
-
 	"github.com/conductorone/baton-greenhouse/pkg/client"
+	"github.com/conductorone/baton-greenhouse/pkg/models"
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 	"github.com/conductorone/baton-sdk/pkg/annotations"
 	"github.com/conductorone/baton-sdk/pkg/pagination"
+	"github.com/conductorone/baton-sdk/pkg/types/entitlement"
+	"github.com/conductorone/baton-sdk/pkg/types/grant"
 	resourceSdk "github.com/conductorone/baton-sdk/pkg/types/resource"
 )
 
@@ -16,46 +17,89 @@ type roleBuilder struct {
 	Client *client.Client
 }
 
+const rolePermissionName = "assigned"
+
 func (b *roleBuilder) ResourceType(_ context.Context) *v2.ResourceType {
 	return roleResourceType
 }
 
-func (b *roleBuilder) List(ctx context.Context, _ *v2.ResourceId, pToken *pagination.Token) ([]*v2.Resource, string, annotations.Annotations, error) {
+func (b *roleBuilder) List(_ context.Context, _ *v2.ResourceId, _ *pagination.Token) ([]*v2.Resource, string, annotations.Annotations, error) {
 	var roleResources []*v2.Resource
 
-	roles, rl, next, err := b.Client.ListRoles(ctx, pToken.Token)
+	siteAdminResource, err := createSiteAdminRoleResource()
 	if err != nil {
-		return nil, "", nil, fmt.Errorf("error while listing roles, error: %w", err)
+		return nil, "", nil, err
 	}
 
-	for _, role := range roles {
-		roleResource, err := parseIntoRoleResource(role)
-		if err != nil {
-			return nil, "", nil, err
-		}
+	roleResources = append(roleResources, siteAdminResource)
 
-		roleResources = append(roleResources, roleResource)
-	}
-
-	var anno annotations.Annotations
-	anno.WithRateLimiting(rl)
-
-	return roleResources, next, anno, nil
+	return roleResources, "", nil, nil
 }
 
 func (b *roleBuilder) Entitlements(_ context.Context, resource *v2.Resource, _ *pagination.Token) ([]*v2.Entitlement, string, annotations.Annotations, error) {
-	return nil, "", nil, nil
+	var roleEntitlements []*v2.Entitlement
+
+	assigmentOptions := []entitlement.EntitlementOption{
+		entitlement.WithGrantableTo(userResourceType),
+		entitlement.WithDescription(resource.Description),
+		entitlement.WithDisplayName(resource.DisplayName),
+	}
+
+	roleEntitlements = append(roleEntitlements, entitlement.NewPermissionEntitlement(resource, rolePermissionName, assigmentOptions...))
+
+	return roleEntitlements, "", nil, nil
 }
 
-func (b *roleBuilder) Grants(ctx context.Context, resource *v2.Resource, pToken *pagination.Token) ([]*v2.Grant, string, annotations.Annotations, error) {
-	return nil, "", nil, nil
+func (b *roleBuilder) Grants(ctx context.Context, resource *v2.Resource, _ *pagination.Token) ([]*v2.Grant, string, annotations.Annotations, error) {
+	var ret []*v2.Grant
+	var err error
+
+	users, err := b.listAllUsers(ctx)
+	if err != nil {
+		return nil, "", nil, err
+	}
+
+	for _, user := range users {
+		if user.SiteAdmin {
+			userResource, err := User2Resource(user, nil)
+			if err != nil {
+				return nil, "", nil, err
+			}
+
+			membershipGrant := grant.NewGrant(resource, rolePermissionName, userResource.Id)
+			ret = append(ret, membershipGrant)
+		}
+	}
+
+	return ret, "", nil, nil
 }
 
-func parseIntoRoleResource(role models.Role) (*v2.Resource, error) {
+func (b *roleBuilder) listAllUsers(ctx context.Context) ([]models.User, error) {
+	var listedUsers []models.User
+	var nextPageToken string
+	for {
+		users, _, nextToken, err := b.Client.ListUsers(ctx, nextPageToken)
+		if err != nil {
+			return nil, fmt.Errorf("error loading users cache, error: %w", err)
+		}
+
+		listedUsers = append(listedUsers, users...)
+
+		if nextToken == "" {
+			break
+		}
+		nextPageToken = nextToken
+	}
+
+	return listedUsers, nil
+}
+
+func createSiteAdminRoleResource() (*v2.Resource, error) {
+	id := "site_admin"
+	name := "Site Admin"
 	profile := map[string]interface{}{
-		"id":        role.ID,
-		"name":      role.Name,
-		"role_type": role.Type,
+		"id":   id,
+		"name": name,
 	}
 
 	roleTraits := []resourceSdk.RoleTraitOption{
@@ -63,9 +107,9 @@ func parseIntoRoleResource(role models.Role) (*v2.Resource, error) {
 	}
 
 	ret, err := resourceSdk.NewRoleResource(
-		role.Name,
+		name,
 		roleResourceType,
-		role.ID,
+		id,
 		roleTraits,
 	)
 	if err != nil {

86fff5aabb6699ebc043f3549713f05f856abba6
Javier David Carnelli <javier.carnelli@conductorone.com>
2025-05-08 02:32:48 -0300

    refactor: user resource cretion refactored. First and Last Name for users fixed. Json field names included to the model

--- pkg/connector/convert.go
+++ pkg/connector/convert.go
@@ -8,47 +8,33 @@ import (
 	"github.com/conductorone/baton-sdk/pkg/types/resource"
 )
 
-func User2Resource(u models.User, p *v2.ResourceId) (*v2.Resource, error) {
+func ParseIntoUserResource(user models.User) (*v2.Resource, error) {
 	profile := map[string]interface{}{
-		"first_name": u.Name,
-		"last_name":  u.LastName,
-		"is_admin":   u.SiteAdmin,
+		"first_name": user.FirstName,
+		"last_name":  user.LastName,
+		"is_admin":   user.SiteAdmin,
 	}
 
 	options := []resource.UserTraitOption{
 		resource.WithUserProfile(profile),
-		resource.WithEmail(u.PrimaryEmailAddress, true),
+		resource.WithEmail(user.PrimaryEmailAddress, true),
 	}
-	if u.Disabled {
+	
+	if user.Disabled {
 		options = append(options, resource.WithStatus(v2.UserTrait_Status_STATUS_DISABLED))
 	} else {
 		options = append(options, resource.WithStatus(v2.UserTrait_Status_STATUS_ENABLED))
 	}
 
-	user, err := resource.NewUserResource(
-		fmt.Sprintf("%s %s", u.Name, u.LastName),
+	userResource, err := resource.NewUserResource(
+		fmt.Sprintf("%s %s", user.Name, user.LastName),
 		userResourceType,
-		u.ID,
+		user.ID,
 		options,
-		resource.WithParentResourceID(p),
 	)
 	if err != nil {
-		return nil, fmt.Errorf("cannot make user resource from user «%s %s» (id «%d»)", u.Name, u.LastName, u.ID)
+		return nil, fmt.Errorf("cannot make user resource from user «%s %s» (id «%d»)", user.Name, user.LastName, user.ID)
 	}
 
-	return user, nil
-}
-
-func Users2Resources(us []models.User, p *v2.ResourceId) ([]*v2.Resource, error) {
-	var users []*v2.Resource
-
-	for _, u := range us {
-		user, err := User2Resource(u, p)
-		if err != nil {
-			return nil, err
-		}
-		users = append(users, user)
-	}
-
-	return users, nil
+	return userResource, nil
 }
--- pkg/models/users.go
+++ pkg/models/users.go
@@ -3,43 +3,44 @@ package models
 import "time"
 
 type User struct {
-	ID                  int
-	Name                string
-	LastName            string
-	PrimaryEmailAddress string
-	UpdatedAt           time.Time
-	CreatedAt           time.Time
-	Disabled            bool
-	SiteAdmin           bool `json:"site_admin,omitempty"`
-	Emails              []string
-	EmployeeID          int
-	LinkedCandidateIDs  []int
-	Offices             []Office
-	Departments         []Department
+	ID                  int          `json:"id"`
+	Name                string       `json:"name"`
+	FirstName           string       `json:"first_name"`
+	LastName            string       `json:"last_name"`
+	PrimaryEmailAddress string       `json:"primary_email_address"`
+	UpdatedAt           time.Time    `json:"updated_at"`
+	CreatedAt           time.Time    `json:"created_at"`
+	Disabled            bool         `json:"disabled"`
+	SiteAdmin           bool         `json:"site_admin,omitempty"`
+	Emails              []string     `json:"emails,omitempty"`
+	EmployeeID          int          `json:"employee_id"`
+	LinkedCandidateIDs  []int        `json:"linked_candidate_ids,omitempty"`
+	Offices             []Office     `json:"offices,omitempty"`
+	Departments         []Department `json:"departments,omitempty"`
 }
 
 type Office struct {
-	ID                     int
-	Name                   string
-	Location               Location
-	PrimaryContactUserID   int
-	ParentID               int
-	ParentOfficeExternalID string
-	ChildIDs               []int
-	ChildOfficeExternalIDs []string
-	ExternalID             string
+	ID                     int      `json:"id,omitempty"`
+	Name                   string   `json:"name,omitempty"`
+	Location               Location `json:"location,omitempty"`
+	PrimaryContactUserID   int      `json:"primary_contact_user_id,omitempty"`
+	ParentID               int      `json:"parent_id,omitempty"`
+	ParentOfficeExternalID string   `json:"parent_office_external_id,omitempty"`
+	ChildIDs               []int    `json:"child_ids,omitempty"`
+	ChildOfficeExternalIDs []string `json:"child_office_external_ids,omitempty"`
+	ExternalID             string   `json:"external_id,omitempty"`
 }
 
 type Department struct {
-	ID                         int
-	Name                       string
-	ParentID                   int
-	ParentDepartmentExternalID string
-	ChildIDs                   []int
-	ChildDeparmentExternalIDs  []string
-	ExternalID                 string
+	ID                         int      `json:"id,omitempty"`
+	Name                       string   `json:"name,omitempty"`
+	ParentID                   int      `json:"parent_id,omitempty"`
+	ParentDepartmentExternalID string   `json:"parent_department_external_id,omitempty"`
+	ChildIDs                   []int    `json:"child_ids,omitempty"`
+	ChildDepartmentExternalIDs []string `json:"child_department_external_ids,omitempty"`
+	ExternalID                 string   `json:"external_id,omitempty"`
 }
 
 type Location struct {
-	Name string
+	Name string `json:"name,omitempty"`
 }

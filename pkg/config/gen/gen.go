ed2807e7fd9079fd778e356b7c08460125d96706
MB Burch <mb.burch@conductorone.com>
2025-05-14 09:51:15 -0600

    Updates for containerization

---
+++ pkg/config/gen/gen.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	cfg "github.com/conductorone/baton-greenhouse/pkg/config"
+	"github.com/conductorone/baton-sdk/pkg/config"
+)
+
+func main() {
+	config.Generate("greenhouse", cfg.Config)
+}
